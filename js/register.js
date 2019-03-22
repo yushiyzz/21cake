6e7651fd9d0c7f9cd4fb359175226fd36c3d4dc4
yushiyzz <710954435@qq.com>
2019-03-23 07:51:47 +0800

    21cake注册页面

---
+++ js/register.js
@@ -0,0 +1,12 @@
+	
+	//手机号码验证
+	var pone=$("#telephone").val();
+	var myreg = /^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\\d{8}$/;
+
+    if(!myreg.test(pone)){
+       $(".err-text span").html("请输入正确的手机号码")
+    }else{
+     	$("#telephone +").css("display","inline");
+     	$(".err-text span").html("");
+    }
+  
